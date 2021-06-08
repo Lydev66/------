fe9ca6e3babede0f753df73d91b97b0e564a7492
Lydev68 <18027118545@163.com>
2021-06-08 16:58:06 +0800

    first commit

---
+++ js/my.effect.js
@@ -0,0 +1,30 @@
+/*
+* page effect js
+* author:liyingxia
+* dateTime: 2020-06-14  01:08
+*
+* */
+
+
+// header nav a
+$(function(){
+    $(".navbar-nav a").mouseover(function(){
+        //鼠标放上去时当前a的其他兄弟元素 颜色为白色
+        $(this).siblings("a").css("color","white");
+        //a 样式为蓝 出现下边宽
+        $(this).css("color","#287fec");
+        $(this).css("border-bottom","2px solid #287fec");
+    });
+
+    //鼠标移出时边框为none ,颜色为白
+    $(".navbar-nav a").mouseout(function(){
+        $(this).css("border-bottom","none");
+        $(this).css("color","white");
+    });
+
+    $(".navbar-nav a").click(function(){
+        $(this).css("color","#287fec");
+        $(this).css("border-bottom","2px solid #287fec");
+    });
+
+});
